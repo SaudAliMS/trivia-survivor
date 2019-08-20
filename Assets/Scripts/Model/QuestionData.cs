5c035e885213294e1fe3f6de75c931d4c0034457
Saud <saud.ali@mindstormstudios.com>
2019-08-20 16:29:49 +0500

    updated file format

--- Assets/Scripts/Model/QuestionData.cs
+++ Assets/Scripts/Model/QuestionData.cs
@@ -6,13 +6,11 @@ using Utils;
 public class QuestionData 
 {
     private int id;
-    private int level;
     private string questionStatement;
     private string answerStatement;
     private bool answerIsTrue;
 
     public int Id                           { get { return id; } }
-    public int Level                        { get { return level; } }
     public string Question                  { get { return questionStatement; } }
     public string AnswerStatement           { get { return answerStatement; } }
     public bool AnswerIsTrue                { get { return answerIsTrue; } }
@@ -21,11 +19,9 @@ public class QuestionData
     {
         QuestionData levelData              = new QuestionData();
         levelData.id                     = Utility.ToInt(elements[0]);
-        levelData.level                  = Utility.ToInt(elements[1]);
-        levelData.answerIsTrue           = Utility.ToInt(elements[2]) == 1 ? true : false;
-        levelData.questionStatement      = (elements[3]);
-        levelData.answerStatement        = (elements[4]);
-
+        levelData.answerIsTrue           = Utility.ToInt(elements[1]) == 1 ? true : false;
+        levelData.questionStatement      = (elements[2]);
+        levelData.answerStatement        = (elements[3]);
         return levelData;
     }
 }
